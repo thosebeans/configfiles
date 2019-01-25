9b6ded796bcff9cac9e273c4ba8289a26c8df65b
Christian <samsepi0l@gmx.de>
2019-01-25 14:22:31 +0100

    automated wento-commit

---
+++ nvim/downloadSummerfruit256.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+mkdir -p ~/.config/nvim/colors
+wget https://www.vim.org/scripts/download_script.php?src_id=10153 -O ~/.config/nvim/colors/summerfruit256.vim
---
+++ nvim/installplug.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+mkdir -p $HOME/.config/nvim/autoload
+wget -O $HOME/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
